a0003f2c3f7c3f8bbad8f6aaf17e2982e70f62de
John Mauney <parkermauney+git@gmail.com>
2015-05-30 14:31:00 -0400

    remove old stuff

--- amazon.py
+++ amazon.py
@@ -22,19 +22,3 @@ def amazon_url(match, say=None):
         description += a.OfferListing.Price.FormattedPrice
 
     say(description)
-
-@hook.command
-def amazon(inp, say=None):
-    '.amazon <query> -- returns the first Amazon search result for <query>'
-    api = API(locale='us')
-    result = api.item_lookup(inp)
-
-    for item in result.Items.Item:
-        description = item.ItemAttributes.Title + " - "
-
-    price = api.item_lookup(inp, ResponseGroup='Offers')
-
-    for a in price.Items.Item.Offers.Offer:
-        description += a.OfferListing.Price.FormattedPrice
-
-    say(description)
